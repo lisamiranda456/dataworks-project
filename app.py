ac2ba6f7a575415dd8260d8ce7f3d07d1075e661
Lisa Sara Miranda <30820152+lisamiranda456@users.noreply.github.com>
2025-02-16 17:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -972,5 +972,5 @@ def run_task():
     # Return the response as JSON
     return jsonify(response)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
     
